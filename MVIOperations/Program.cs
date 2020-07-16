1924202199675c5d652742d36aca98d16038a1b6
xamlware <jbaird@outlook.com>
2020-07-16 11:39:29 -0400

    * Program.cs: * _Imports.razor: * Login.razor: * Index.razor: * MainLayout.razor:
    * MVIOperations.csproj: Add Blazored.LocalStorage
    
    * app.css: Add localstorage

--- MVIOperations/Program.cs
+++ MVIOperations/Program.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Blazored;
+using Blazored.LocalStorage;
 
 namespace MVIOperations
 {
@@ -18,6 +16,8 @@ namespace MVIOperations
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+
 
             await builder.Build().RunAsync();
         }
